7463b9e32061515792e09dbc1d5795e371bbc6f3
vedantshetti <vedantshetti123456@gmail.com>
2025-03-07 12:15:47 +0530

    added all the services login and injected them in the controllers

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+// ParseIntPipe is a pipe that will parse the parameter to an integer
+
+import { UsersService } from './users.service';
 
 @Controller('users') // these will handle the /users route
 export class UsersController {
@@ -9,14 +22,16 @@ export class UsersController {
   // patch /users/:id
   // delete /users/:id
 
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // this will handle the get /users route to get all users
-  findAll() {
-    return []; // return all users
+  findAll(@Query('role') role?: 'INTERN' | 'EMPLOYEES' | 'ADMIN') {
+    return this.usersService.findAll(role); // return all users
   }
 
   @Get(':id') // this will handle the get /users/:id route to get a single user
-  findOne(@Param('id') id: string) {
-    return { id }; // return a single user
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id); // return a single user
   }
 
   // also remember that order of the routes matters if we have same method request
@@ -29,17 +44,32 @@ export class UsersController {
   }
 
   @Post() // this will handle the post /users route to create a new user
-  create(@Body() user: {}) {
-    return user; // return a message that user has been created
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'EMPLOYEES' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user); // return a message that user has been created
   }
 
   @Patch(':id') // this will handle the patch /users/:id route to update a user
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate }; // return a single user
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'EMPLOYEES' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(id, updatedUser); // return a single user
   }
 
   @Delete(':id') // this will handle the delete /users/:id route to delete a user
-  delete(@Param('id') id: string) {
-    return { id }; // return a single user
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id); // return a single user
   }
 }
--- lesson01/src/users/users.service.ts
+++ lesson01/src/users/users.service.ts
@@ -1,4 +1,89 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Alice Johnson',
+      email: 'alice.johnson@example.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Bob Smith',
+      email: 'bob.smith@example.com',
+      role: 'DEVELOPER',
+    },
+    {
+      id: 3,
+      name: 'Catherine Brown',
+      email: 'catherine.brown@example.com',
+      role: 'MANAGER',
+    },
+    {
+      id: 4,
+      name: 'David Wilson',
+      email: 'david.wilson@example.com',
+      role: 'DESIGNER',
+    },
+    {
+      id: 5,
+      name: 'Emily Davis',
+      email: 'emily.davis@example.com',
+      role: 'INTERN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'EMPLOYEES' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users; // if no role is provided, return all users
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'EMPLOYEES' | 'ADMIN';
+  }) {
+    const usersByHighestId = this.users.sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'EMPLOYEES' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          ...updatedUser,
+        };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removeduser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removeduser;
+  }
+}
