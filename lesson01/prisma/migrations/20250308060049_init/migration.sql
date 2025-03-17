62ddd9497fcc814fe48694fb4e385d2a556ad14d
vedantshetti <vedantshetti123456@gmail.com>
2025-03-17 17:05:52 +0530

    added the prisma in the file and used resources

---
+++ lesson01/prisma/migrations/20250308060049_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('INTERN', 'ADMIN', 'EMPLOYEES');
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
